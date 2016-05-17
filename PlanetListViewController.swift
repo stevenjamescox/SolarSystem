d71ba23778a46ba26d87ad7201d6fd4c9c2dcbd3
Steve Cox <coxsteve@gmail.com>
2016-05-17 15:47:19 -0600

    SMH constraints

--- PlanetListViewController.swift
+++ PlanetListViewController.swift
@@ -9,33 +9,36 @@
 import UIKit
 
 class PlanetListViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return PlanetController.planets.count
     }
-
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 53
+    }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("planetCell", forIndexPath: indexPath)
@@ -44,21 +47,20 @@ class PlanetListViewController: UITableViewController {
         cell.textLabel?.textAlignment = .Center
         cell.imageView?.contentMode = .Redraw
         cell.imageView?.image = UIImage(named: planetInfo.imageName)
-        cell.detailTextLabel?.text = "planet \(indexPath.row + 1) from the Sun"
-
+        cell.detailTextLabel?.text = "planet \(indexPath.row + 1) of 9"
+        
         return cell
     }
     
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "toPlanetDetailSegue"{
-        
+            let planetDetailViewController = segue.destinationViewController as? PlanetDetailViewController
+            if let selectedPlanet = self.tableView.indexPathForSelectedRow {
+                let planet = PlanetController.planets[selectedPlanet.row]
+                planetDetailViewController?.planet = planet
+            }
         }
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    
-
 }
