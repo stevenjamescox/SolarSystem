a53c7c1725851c52295ebca0f7d85a61e35d1924
Steve Cox <coxsteve@gmail.com>
2016-05-17 16:02:51 -0600

    finally got constraints to stop fighting me

--- PlanetListViewController.swift
+++ PlanetListViewController.swift
@@ -12,12 +12,6 @@ class PlanetListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
     override func didReceiveMemoryWarning() {
@@ -27,12 +21,10 @@ class PlanetListViewController: UITableViewController {
     // MARK: - Table view data source
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return PlanetController.planets.count
     }
     
