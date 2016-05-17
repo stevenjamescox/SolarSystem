b888beb422a0ff9db9909e6737478f0b3213b102
Steve Cox <coxsteve@gmail.com>
2016-05-17 16:19:11 -0600

    beginnings of adding average temp data

--- SolarSystem/Planet.swift
+++ SolarSystem/Planet.swift
@@ -15,14 +15,16 @@ class Planet {
     let diameter: Int
     let dayLength: Float
     let millionKMsFromSun: Float
+    let averageTemp: Int
     
-    init(name: String, diameter: Int, dayLength: Float, millionKMsFromSun: Float) {
+    init(name: String, diameter: Int, dayLength: Float, millionKMsFromSun: Float, averageTemp: Int) {
         
         self.name = name
         self.imageName = name.lowercaseString
         self.diameter = diameter
         self.dayLength = dayLength
         self.millionKMsFromSun = millionKMsFromSun
+        self.averageTemp = averageTemp
         
     }
     
--- SolarSystem/PlanetController.swift
+++ SolarSystem/PlanetController.swift
@@ -11,23 +11,23 @@ import Foundation
 class PlanetController {
     
     static var planets: [Planet] {
-        let mercury = Planet(name: "Mercury", diameter: 4879, dayLength: 4222.6, millionKMsFromSun: 57.9)
+        let mercury = Planet(name: "Mercury", diameter: 4879, dayLength: 4222.6, millionKMsFromSun: 57.9, averageTemp: 332)
         
-        let venus = Planet(name: "Venus", diameter: 12104, dayLength: 2802, millionKMsFromSun: 108.2)
+        let venus = Planet(name: "Venus", diameter: 12104, dayLength: 2802, millionKMsFromSun: 108.2, averageTemp: 867)
         
-        let earth = Planet(name: "Earth", diameter: 12756, dayLength: 24, millionKMsFromSun: 149.6)
+        let earth = Planet(name: "Earth", diameter: 12756, dayLength: 24, millionKMsFromSun: 149.6, averageTemp: 59)
         
-        let mars = Planet(name: "Mars", diameter: 6792, dayLength: 24.7, millionKMsFromSun: 227.9)
+        let mars = Planet(name: "Mars", diameter: 6792, dayLength: 24.7, millionKMsFromSun: 227.9, averageTemp:-82)
         
-        let jupiter = Planet(name: "Jupiter", diameter: 142984, dayLength: 9.9, millionKMsFromSun: 778.6)
+        let jupiter = Planet(name: "Jupiter", diameter: 142984, dayLength: 9.9, millionKMsFromSun: 778.6, averageTemp: -162)
         
-        let saturn = Planet(name: "Saturn", diameter: 120536, dayLength: 10.7, millionKMsFromSun: 1433.5)
+        let saturn = Planet(name: "Saturn", diameter: 120536, dayLength: 10.7, millionKMsFromSun: 1433.5, averageTemp: -218)
         
-        let uranus = Planet(name: "Uranus", diameter: 51118, dayLength: 17.2, millionKMsFromSun: 2872.5)
+        let uranus = Planet(name: "Uranus", diameter: 51118, dayLength: 17.2, millionKMsFromSun: 2872.5, averageTemp: -323)
         
-        let neptune = Planet(name: "Neptune", diameter: 49528, dayLength: 16.1, millionKMsFromSun: 4495.1)
+        let neptune = Planet(name: "Neptune", diameter: 49528, dayLength: 16.1, millionKMsFromSun: 4495.1, averageTemp: -330)
         
-        let pluto = Planet(name: "Pluto", diameter: 2370, dayLength: 153.3, millionKMsFromSun: 5906.4)
+        let pluto = Planet(name: "Pluto", diameter: 2370, dayLength: 153.3, millionKMsFromSun: 5906.4, averageTemp: -370)
         
         return [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto]
     }
