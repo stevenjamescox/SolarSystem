e30ab24f51ab96eab9477052f3a3d750bfc95cc9
Steve Cox <coxsteve@gmail.com>
2016-05-17 14:48:42 -0600

    progress

--- PlanetListViewController.swift
+++ PlanetListViewController.swift
@@ -39,46 +39,24 @@ class PlanetListViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("planetCell", forIndexPath: indexPath)
+        let planetInfo = PlanetController.planets[indexPath.row]
+        cell.textLabel?.text = planetInfo.name
+        cell.imageView?.contentMode = .Redraw
+        cell.imageView?.image = UIImage(named: planetInfo.imageName)
+        cell.detailTextLabel?.text = "planet \(indexPath.row + 1)"
 
-        // Configure the cell...
+
 
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
     
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     if segue.identifier == "toPlanetDetailSegue"
+        if segue.identifier == "toPlanetDetailSegue"{
+        
+        }
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
