e1da41b3ef1042b3a4ae3051a24fc85af6748e75
Steve Cox <coxsteve@gmail.com>
2016-05-17 14:24:24 -0600

    reuse & segue identifier corrections

--- PlanetListViewController.swift
+++ PlanetListViewController.swift
@@ -22,24 +22,23 @@ class PlanetListViewController: UITableViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return PlanetController.planets.count
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("toPlanetDetailSegue", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("planetCell", forIndexPath: indexPath)
 
         // Configure the cell...
 
@@ -74,14 +73,15 @@ class PlanetListViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     if segue.identifier == "toPlanetDetailSegue"
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
