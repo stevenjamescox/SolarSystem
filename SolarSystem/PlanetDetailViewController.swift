674c85c4d8f48045d91cd2cc160fee3b7cc54ee5
Steve Cox <coxsteve@gmail.com>
2016-05-17 15:09:22 -0600

    configuring updateWithPlanet function

--- SolarSystem/PlanetDetailViewController.swift
+++ SolarSystem/PlanetDetailViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class PlanetDetailViewController: UIViewController {
 
+    @IBOutlet weak var planetNameLabel: UILabel!
+    
+    @IBOutlet weak var planetImageSpot: UIImageView!
+    
+    @IBOutlet weak var planetDayLengthLabel: UILabel!
+    
+    @IBOutlet weak var planetDistanceFromSunLabel: UILabel!
+    
+    @IBOutlet weak var planetDiameterLabel: UILabel!
+    
+    var planet: Planet?
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let planet = planet{
+            updateWithPlanet(planet)
+             super.viewDidLoad()
+            
+        }
+    }
+    
+    func updateWithPlanet(planet: Planet){
+    
+        planetNameLabel.text = planet.name
+        planetImageSpot.image = UIImage(named: planet.imageName)
+        planetDayLengthLabel.text = "Length of day: \(planet.dayLength)"
+        planetDistanceFromSunLabel.text = "Distance from Sun: \(planet.millionKMsFromSun)"
+        planetDiameterLabel.text = "Diameter: \(planet.diameter)"
     }
 
     override func didReceiveMemoryWarning() {
